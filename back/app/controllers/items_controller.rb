668f15ecf3d908292ac6548450440e820e90ec49
tom1236908745 <tom1233908745@gmail.com>
2022-01-02 17:23:09 +0900

    一覧に詳細リンクを付けた

--- back/app/controllers/items_controller.rb
+++ back/app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  def show
+    @item = Item.find_by(params[:id])
+    
+  end
 end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
   get 'items/index' => "items#index"
+  get 'items/:id' => "items#show"
   get '/' => "home#top"
   get 'about' => "home#about"
+  
 end
