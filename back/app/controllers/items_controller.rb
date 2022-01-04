e6f8fde02806597ef3877a0d21da372fab11011c
tom1236908745 <tom1233908745@gmail.com>
2022-01-04 14:00:30 +0900

    add 新規出品機能

--- back/app/controllers/items_controller.rb
+++ back/app/controllers/items_controller.rb
@@ -1,9 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: :desc)
   end
   def show
     @item = Item.find_by(params[:id])
     
+  end
+
+  def new
+    
+  end
+  def create
+    @item = Item.new(content: params[:content])
+    @item.save
+    redirect_to("/items/index")
   end
 end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   get 'items/index' => "items#index"
+  get 'items/new' => "items#new"
+  post 'items/create' => "items#create"
   get 'items/:id' => "items#show"
   get '/' => "home#top"
   get 'about' => "home#about"
