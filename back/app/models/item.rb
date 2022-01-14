2cb28ad07a05ddac6b63eace3a4e662bcbf2f8b5
tom1236908745 <tom1233908745@gmail.com>
2022-01-14 14:33:43 +0900

    AWS s3の設定

--- back/app/models/item.rb
+++ back/app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  has_one_attached :image
   validates :content, {presence: true, length: {maximum: 20}}
   validates :image_name, {presence: true}
   validates :user_id, {presence: true}
